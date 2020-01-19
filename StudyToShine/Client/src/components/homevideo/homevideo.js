7eeaf1a98ede98f84c856897acc0a3b0ff1b5995
Rohit Lal <rohitlal1155@gmail.com>
2020-01-19 10:43:57 +0530

    Update change related with home video

--- StudyToShine/Client/src/components/homevideo/homevideo.js
+++ StudyToShine/Client/src/components/homevideo/homevideo.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
 import '../site.css';
+import API from "../../utils/api";
 
 export default class HomeVideo extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+     videodetail: null
+    };
+  }
+  async componentDidMount() {
+    // Load async data.
+    let userData = await API.get('/videos');
+    if(userData)
+    {
+      this.setState({videourl : userData.data})
+      console.log(userData.data);
+    }
+  }
     render()
     {
+      let url1, url2 ,url3;
+      if(this.state.videourl)
+      {
+        url1 = <video src={this.state.videourl[0].url} type="video/mp4" controls height="150" width="418">
+        Your browser doesn't support videos!
+       </video>;
+       url2 = <video src={this.state.videourl[1].url} type="video/mp4" controls height="150" width="418">
+       Your browser doesn't support videos!
+      </video>;
+      url3 = <video src={this.state.videourl[2].url} type="video/mp4" controls height="150" width="418">
+      Your browser doesn't support videos!
+     </video>;
+      } 
         return(
             <div>
              <section>
@@ -13,9 +44,7 @@ export default class HomeVideo extends Component {
         <div className="row text-center mb-5">
           <div className="col-lg-4 col-md-4 col-sm-12">
             <div className="card box-shadow-custom">
-                <video src="https://res.cloudinary.com/demo/video/upload/dog.mp4" type="video/mp4" controls height="150" width="418">
-                  Your browser doesn't support videos!
-                 </video>
+                {url1}
               <div className="card-body">
                 <h4 className="card-title">John Doe</h4>
                 <p className="card-text">Some example text.</p>                
@@ -24,9 +53,7 @@ export default class HomeVideo extends Component {
           </div>
           <div className="col-lg-4 col-md-4 col-sm-12">
             <div className="card box-shadow-custom">              
-              <video src="https://res.cloudinary.com/demo/video/upload/dog.mp4" type="video/mp4" controls height="150" width="418">
-               Your browser doesn't support videos!
-              </video>
+             {url2}
               <div className="card-body">
                 <h4 className="card-title">John Doe</h4>
                 <p className="card-text">Some example text.</p>                
@@ -35,9 +62,7 @@ export default class HomeVideo extends Component {
           </div>
           <div className="col-lg-4 col-md-4 col-sm-12">
             <div className="card box-shadow-custom">
-                <video src="https://res.cloudinary.com/demo/video/upload/dog.mp4" type="video/mp4" controls height="150" width="418">
-                  Your browser doesn't support videos!
-                 </video>
+                {url3}
               <div className="card-body">
                 <h4 className="card-title">John Doe</h4>
                 <p className="card-text">Some example text.</p>                
--- StudyToShine/Client/src/components/login/login.js
+++ StudyToShine/Client/src/components/login/login.js
@@ -1,8 +1,59 @@
 import React from 'react';  
 import '../site.css';  
 import { Button } from 'reactstrap';
+import API from "../../utils/api";
 
 class Login extends React.Component {  
+
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+         token: null,
+         value:'',
+         passwd: ''
+        };
+        this.checkUser = this.checkUser.bind(this);
+        this.handleEmailChange= this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+      }
+
+      async checkUser() {
+        // Load async data.
+        console.log('email' + this.state.value + 'password' + this.state.passwd);
+        let data = {
+            email: this.state.value,
+            password: this.state.passwd
+        };
+        let isUserExist;
+        try
+        {
+         isUserExist = await API.post('/user/login', data);
+        }
+        catch(ex)
+        {
+            return alert('login failed');
+        }
+        if(isUserExist && isUserExist.data && isUserExist.data.status === "success")
+        {
+          this.setState({token : isUserExist.data.data.token})
+          console.log('token' + isUserExist.data.data.token);
+          alert('LoginSuccessfull');
+          
+        }
+        else
+        {
+            alert('LoginFailed Please Check login id and password !!')
+        }
+      }
+      handleEmailChange(event)
+      {
+        this.setState({value: event.target.value});
+      }
+      handlePasswordChange(event)
+      {
+        this.setState({passwd: event.target.value});
+      }
   render() {  
 return (  
 <div className='popup'>  
@@ -14,19 +65,19 @@ return (
               </div>
               <div className="modal-body">
                     <div className="form-group">
-                      <label for="email">Email address:</label>
-                      <input type="email" className="form-control" id="email" autocomplete="off"/>
+                      <label >Email address:</label>
+                      <input type="email" className="form-control" id="email" value={this.state.value} onChange={this.handleEmailChange}/>
                     </div>
                     <div className="form-group">
-                      <label for="pwd">Password:</label>
-                      <input type="password" className="form-control" id="pwd"/>
+                      <label >Password:</label>
+                      <input type="password" className="form-control" id="pwd" value={this.state.passwd} onChange={this.handlePasswordChange}/>
                     </div>
                     <div className="form-group form-check">
                       <label className="form-check-label">
                         <input className="form-check-input" type="checkbox"/> Remember me
                       </label>
                     </div>
-                    <button type="submit" className="btn button-color">Register</button>
+                    <button type="submit" className="btn button-color" onClick={this.checkUser}>Register</button>
               </div>
 
 </div>  
