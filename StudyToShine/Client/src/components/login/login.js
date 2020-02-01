f2df710d09fac63c8d35414431a5a749e7d0dc2d
Gaurav Gupta <gauravg07@hotmail.com>
2020-02-01 15:56:30 +0530

    login popup changes

--- StudyToShine/Client/src/components/login/login.js
+++ StudyToShine/Client/src/components/login/login.js
@@ -56,32 +56,52 @@ class Login extends React.Component {
       }
   render() {  
 return (  
-<div className='popup'>  
-<div className='popup\_inner inner-background'>  
-<h1>{this.props.text}</h1>  
-<div className="modal-header header-color">
-                <h4 className="modal-title">Signup</h4>
-                <Button onClick={this.props.closePopup} close></Button>  
-              </div>
-              <div className="modal-body">
-                    <div className="form-group">
-                      <label >Email address:</label>
-                      <input type="email" className="form-control" id="email" value={this.state.value} onChange={this.handleEmailChange}/>
-                    </div>
-                    <div className="form-group">
-                      <label >Password:</label>
-                      <input type="password" className="form-control" id="pwd" value={this.state.passwd} onChange={this.handlePasswordChange}/>
-                    </div>
-                    <div className="form-group form-check">
-                      <label className="form-check-label">
-                        <input className="form-check-input" type="checkbox"/> Remember me
-                      </label>
-                    </div>
-                    <button type="submit" className="btn button-color" onClick={this.checkUser}>Register</button>
-              </div>
-
-</div>  
-</div> 
-
+         <div>
+           <div className='popup'>
+             <div className='popup\_inner-background'>
+  <h1>{this.props.text}</h1>
+             <div> 
+              <div className="modal-dialog modal-dialog-centered">
+                <div className="modal-content">
+                  <div className="modal-header">
+                     <h4 className="modal-title">Sign In</h4>
+                    {/* <button type="button" className="close" data-dismiss="modal">&times;</button> */}
+                    <Button onClick={this.props.closePopup} close></Button>  
+                  </div>
+                  <div className="modal-body">
+                         <div className="form-group">
+                           <label htmlFor="email">Email ID:</label>
+                           <input type="email" className="form-control" id="email" autoComplete="off" value={this.state.value} onChange={this.handleEmailChange}/>
+                         </div>
+                         <div className="form-group">
+                           <label htmlFor="pwd">Password:</label>
+                           <input type="password" className="form-control" id="pwd" value={this.state.passwd} onChange={this.handlePasswordChange}/>
+                         </div>
+                         <div className="form-group form-check">
+                           <label className="form-check-label">
+                             <input className="form-check-input" type="checkbox"/> Keep Me Signed In
+                           </label>
+                         </div>
+                         <div className="form-group">
+                            <button type="submit" id="button-color" value="SIGN IN" className="btn" onClick={this.checkUser}>SIGN IN</button>                        
+                            <a href="#" className="btn button-color" id="button-color" data-toggle="modal" data-backdrop="static" data-target="#signupPopupBox" data-keyboard="false">Register Now</a>
+                         </div>
+                         <a href="#">Forgot Password?</a>
+                          
+                         <div className="cust-social-log-wrap">
+                             <div className="form-group text-center">OR CONTINUE WITH</div>
+                             <div className="">
+                                 <a className="social-link-background" href="#" id="facebookWidget"><img alt="fb-logo" src="https://ii1.pepperfry.com/images/svg/social-login-fb-logo.svg"/><span>Facebook</span></a>
+                                 <a className="social-link-background" href="#" id="googleWidget"><img alt="gmail-logo" src="https://ii1.pepperfry.com/images/svg/social-login-google-logo.svg"/><span>Google</span></a>
+                             </div>
+                              {/* <div className="cust-social-error" style="display: none;"></div> */}
+                         </div>                      
+                  </div>
+               </div>
+             </div>
+          </div>  
+         </div>
+         </div>
+         </div>
 )}}
 export default Login;
